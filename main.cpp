7e4f4161eaf375917ade8f2060c9bf382d5a3c78
VIVEK <viveky7982@gmail.com>
2026-07-23 14:54:26 +0530

    Added main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
+
 #include "include/Crawler.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
 
 int main()
 {
-    Crawler crawler;
+Crawler crawler;
+std::string url;
 
-    crawler.crawl("http://quotes.toscrape.com");
+std::cout << "=====================================\n";
+std::cout << " Simple Web Crawler\n";
+std::cout << "=====================================\n";
+std::cout << "Enter the Seed URL\n";
+std::cout << "Format: https://example.com\n";
+std::cout << "Example: https://quotes.toscrape.com\n\n";
+std::cout << "URL: ";
 
-    return 0;
+std::getline(std::cin, url);
+
+// Convert user input to lowercase
+std::transform(url.begin(), url.end(), url.begin(),
+[](unsigned char c)
+{
+return std::tolower(c);
+});
+
+if (url.empty())
+{
+std::cout << "\nError: URL cannot be empty.\n";
+return 1;
 }
 
+crawler.crawl(url);
+
+return 0;
+}
